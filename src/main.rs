971fb075bdc4ef02493d00c0cad1a3912507c7f9
Ryan Hoy <ryan.s.hoy@gmail.com>
2023-09-03 19:49:10 -0400

    Solved the previous issue by refactoring and found a new one as I am trying to molualarize.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use chrono::NaiveDate;
 use serde::{Deserialize, Serialize};
-use serde_json::{ to_string_pretty, to_writer, Result};
-use std::error::Error;
+use serde_json::to_writer;
+use std::fmt::Display;
 use std::fs::File;
+use std::path::{Path, self};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Comment {
@@ -45,33 +46,48 @@ impl Task {
 }
 
 fn main() {
-    let task = Task::new();
-    if let Ok(json_str) = to_string_pretty(&task) {
-        println!("{}", json_str);
-    } else {
-        println!("Failed to serialize JSON.");
-    }
-    write_tasks(task);
+    let path = Path::new("data_file.json");
+    //let display = path.display();
+
+    //let task = Task::new();
+    //let file = match File::create(&path) {
+    //    Err(why) => panic!("Couldn't create {}: {}", display, why),
+    //    Ok(file) => file,
+    //};
+
+    //match to_writer(&file, &task) {
+    //    Err(why) => panic!("Couldn't write to {}: {}", display, why),
+    //    Ok(_) => println!("Wrote to {}", display),
+    //}
+    //match file.write_all(task) {
+    //    Err(why) => panic!("Couldn't write to {}: {}", display, why),
+    //    Ok(_) => println!("Wrote to {}", display),
+    //}
+    create_task(path);
 }
 
-fn write_tasks(task: Task) -> Result<(), Box<dyn Error>> {
-    let file_path = "data.json";
-    let file = match File::create(file_path) {
+fn create_task(path: Path) {
+    // add ability to append instead of just overwriting file
+    let display = path.display();
+    let task = Task::new();
+    let file = match File::create(&path) {
+        Err(why) => panic!("Couldn't create {}: {}", display, why),
         Ok(file) => file,
-        Err(err) => {
-            eprintln!("Error creating file: {}", err);
-            return Err(err.into());
-        }
     };
 
     match to_writer(&file, &task) {
-        Ok(()) => {
-            println!("JSON data written to {}", file_path);
-            Ok(())
-        }
-        Err(err) => {
-            eprintln!("Error writing JSON data: {}", err);
-            Err(err.into())
-        }
+        Err(why) => panic!("Couldn't write to {}: {}", display, why),
+        Ok(_) => println!("Wrote to {}", display),
     }
 }
+// Menu
+// create new task
+// show tasks - filtering (to-do, complete, new, due today, etc.)
+//   ID Title {Due Date} {# Comments}
+// view task details
+// update task - add due date, add comment
+// delete task
+// verfiy json
+
+// Add CLI options for menu functionality
+// Add created date, modified date
